c18f97a270c02585eb33a3ac115b6efe9d170fa9
dude <masterkorea01@naver.com>
2022-01-01 23:21:04 +0900

    Signup Serializer
    회원가입

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from . import views as accounts_views
+
 app_name = "accounts"
 
-urlpatterns = []
+urlpatterns = [
+    path("signup/", accounts_views.SignupView.as_view()),
+]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,18 @@
+from os import set_inheritable
 from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import serializers
 
-# Create your views here.
+
+class SignupView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, format=None):
+        serializer = serializers.SignupSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
