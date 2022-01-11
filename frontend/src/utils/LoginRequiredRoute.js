6b11bc41981846c4777f4612e5d59998a7b0abaa
dude <masterkorea01@naver.com>
2022-01-11 20:42:07 +0900

    clean code

--- frontend/src/utils/LoginRequiredRoute.js
+++ frontend/src/utils/LoginRequiredRoute.js
@@ -5,9 +5,6 @@ import Login from '../pages/accounts/Login';
 const LoginRequiredRoute = ({children}) => {
     const isAuthenticated = window.localStorage.getItem("isAuthenticated");
 
-    console.log("isAuthenticated", isAuthenticated==="false");
-    console.log("isAuthenticated", typeof(isAuthenticated));
-
     return !isAuthenticated ? (
         <Navigate to="/login"/>
     ) : (
